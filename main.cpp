3162f5b12d829dc4b071f102fe393141093e6722
MxMDB <mxmdb@dbs.snu.ac.kr>
2021-05-26 10:21:19 +0900

    T1, T2, T5

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include "src/Example/mongodb_example.h"
 #include "src/Scenarios/PolyglotDB/Healthcare_Tasks.h"
 #include "src/Scenarios/PolyglotDB/Ecommerce_Tasks.h"
+#include <chrono>
 
 using json = nlohmann::json;         // for convenience
 
@@ -33,6 +34,33 @@ void sample_connection(){
     mongodb_example();
 }
 
+using std::chrono::high_resolution_clock;
+using std::chrono::duration_cast;
+using std::chrono::duration;
+using std::chrono::milliseconds;
+
+template <typename T>
+void timer( void (*f)(T), T x){
+    cout << "======================= TASK START =========================" << endl;
+    auto t1 = high_resolution_clock::now();
+    (*f)(x);
+    auto t2 = high_resolution_clock::now();
+    auto ms_int = duration_cast<milliseconds>(t2 - t1);
+    cout << "ELAPSED TIME: " << ms_int.count() << " ms" << endl;
+    cout << "======================= TASK COMPLETE ======================\n" << endl;
+
+}
+void timer( void (*f)()){
+    cout << "======================= TASK START =========================" << endl;
+    auto t1 = high_resolution_clock::now();
+    (*f)();
+    auto t2 = high_resolution_clock::now();
+    auto ms_int = duration_cast<milliseconds>(t2 - t1);
+    cout << "ELAPSED TIME: " << ms_int.count() << " ms" << endl;
+    cout << "======================= TASK COMPLETE ======================\n" << endl;
+
+}
+
 int main() {
 
 
@@ -41,8 +69,9 @@ int main() {
 
 
     /* M2Bench */
-    T1();
-    T6(9);
+    timer(T1);
+    timer(T2);
+    timer(T6, 9);
    return 0;
 }
 
