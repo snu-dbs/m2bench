6a22aaf9d8989db52da4aabf6473ff007f4d3298
MxMDB <mxmdb@dbs.snu.ac.kr>
2021-05-28 16:27:47 +0900

    delete using namespace mysqlx

--- include/Connection/ScidbPrimitives.h
+++ include/Connection/ScidbPrimitives.h
@@ -7,9 +7,8 @@
 
 
 typedef map<string, any> ScidbData;
-
 typedef struct ScidbDim {
-    string name{};
+    std::string name{};
     int start, end;
     int overlap, interval;
 
@@ -18,10 +17,10 @@ typedef struct ScidbDim {
 } ScidbDim;
 
 typedef struct ScidbAttr {
-    string name{};
-    string type{};
+    std::string name{};
+    std::string type{};
 
-    ScidbAttr(string name, string type): name(name), type(type) {}
+    ScidbAttr(std::string name, std::string type): name(name), type(type) {}
 } ScidbAttr;
 
 typedef struct ScidbSchema {
@@ -41,10 +40,10 @@ typedef struct ScidbArr {
     ~ScidbArr() {
         if (format == DENSE) {      // what the dense array connector made is deleted by the connector
             for (auto &attr : schema.attrs) {
-                if (attr.type.find("float") != string::npos) delete[] any_cast<float*>(data[attr.name]);
-                else if (attr.type.find("double") != string::npos) delete[] any_cast<double*>(data[attr.name]);
-                else if (attr.type.find("int") != string::npos) delete[] any_cast<int*>(data[attr.name]);
-                else if (attr.type.find("string") != string::npos) delete[] any_cast<string*>(data[attr.name]);
+                if (attr.type.find("float") != std::string::npos) delete[] any_cast<float*>(data[attr.name]);
+                else if (attr.type.find("double") != std::string::npos) delete[] any_cast<double*>(data[attr.name]);
+                else if (attr.type.find("int") != std::string::npos) delete[] any_cast<int*>(data[attr.name]);
+                else if (attr.type.find("string") != std::string::npos) delete[] any_cast<std::string*>(data[attr.name]);
             }
         }
     }
