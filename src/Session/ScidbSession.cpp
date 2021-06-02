5d25b813d141feb8a41aba77f7691b6793177a1b
Kyoseung Koo <koo@dbs.snu.ac.kr>
2021-06-02 14:21:01 +0900

    ScidbConnector INT64

--- src/Session/ScidbSession.cpp
+++ src/Session/ScidbSession.cpp
@@ -125,7 +125,8 @@ ScidbSchema ScidbSession::parsingSchema(const string& basicString) {
         // TODO 8, 16, 32, 64 bit primitive types
         if (nat.at(1).find("float") != string::npos) type = FLOAT;
         else if (nat.at(1).find("double") != string::npos) type = DOUBLE;
-        else if (nat.at(1).find("int") != string::npos) type = INT32;
+        else if (nat.at(1).find("int32") != string::npos) type = INT32;
+        else if (nat.at(1).find("int64") != string::npos) type = INT64;
         else if (nat.at(1).find("string") != string::npos) type = STRING;
 
         ScidbAttr attr1(nat.at(0), type);
