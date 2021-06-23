0b6a091f70cf1eddba570793295c707d6faef95f
chloe <chloek409@dbs.snu.ac.kr>
2021-06-23 18:25:13 +0900

    T10 edited according to Disaster schema changes

--- main.cpp
+++ main.cpp
@@ -62,21 +62,30 @@ int main() {
     /* M2Bench */
 //    string curdate = "2021-06-01";
 //    T4();
-    timer(T2);
-    string curdate = "2018-07-07";//"2021-12-25";
-    timer(T3, curdate);
-    //timer(T4,1000,10);
-//     timer(T6, 9);
+
+//    timer(T2);
+
+//    string curdate = "2018-07-07";//"2021-12-25";
+//    timer(T3, curdate);
+
+//    timer(T4,1000,10);
 
 //    string pid = "B007SYGLZO";
 //    string curdate = "2021-06-01";
 //    timer(T5,pid, curdate);
 
-//    timer(T6, 9);
+//     timer(T6, 9);
 
 //    timer(T8,9);
 
 //    timer(T9);
+
+    string ts1 = "2020-05-01 00:00:00";
+    string ts2 = "2020-06-01 00:00:00";
+    timer(T10,ts1,ts2);
+//
+//    int earthquake_X_id = 41865;
+//    timer(T11, earthquake_X_id);
 
 //    string ts1 = "2020-09-16 00:00:00";
 //    string ts2 = "2020-09-16 01:00:00";
@@ -85,12 +94,6 @@ int main() {
 //    long ts = 1600182000+10800*3.5;
 //    timer(T16,ts);
 
-//    string ts1 = "2020-05-01 00:00:00";
-//    string ts2 = "2020-07-01 00:00:00";
-//    timer(T10,ts1,ts2);
-//
-//    int earthquake_X_id = 41865;
-//    timer(T11, earthquake_X_id);
 
 //     timer(T14, 0, 60);
 
