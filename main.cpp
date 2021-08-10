659e69ca0564c00ae2b4094cf3b0c2fa63ccca51
MxMDB <mxmdb@dbs.snu.ac.kr>
2021-08-10 10:57:58 +0900

    scaling bp edge

--- main.cpp
+++ main.cpp
@@ -134,11 +134,18 @@ void scalingData(int SF){
 
 
 
-
+    cout << "Scale InterestedIn ..." << endl;
+    auto ScalingInterestedIn = DataScaler();
+    ScalingInterestedIn.readSchema("/home/mxmdb/CLionProjects/m2bench_ao/schema/interested_schema.json");
+    ScalingInterestedIn.scaleBipartiteEdge("/home/mxmdb/m2bench/data/ecommerce/property_graph/person_interestedIn_tag.csv", SF, ",");
 }
 
 int main(int argc, char *argv[]) {
 
+    cout << "Scale InterestedIn ..." << endl;
+    auto ScalingInterestedIn = DataScaler();
+    ScalingInterestedIn.readSchema("/home/mxmdb/CLionProjects/m2bench_ao/schema/interested_schema.json");
+    ScalingInterestedIn.scaleBipartiteEdge("/home/mxmdb/m2bench/data/ecommerce/property_graph/person_interestedIn_tag.csv", 5 , ",");
 //    scalingData(2);
 
 
@@ -151,92 +158,58 @@ int main(int argc, char *argv[]) {
 
 //    string T10_ts1 = "2020-06-01 00:00:00";
 //    string T10_ts2 = "2020-06-01 02:00:00";
+////    cout<<"input: " <<T10_ts1 << ", " << T10_ts2<<endl;
 //    timer(T10, T10_ts1, T10_ts2);
 
 //    int earthquake_X = 41862;
 //    timer(T11, earthquake_X);
 
-//    string T12_ts1 = "2020-09-17 00:00:00";
-//    string T12_ts2 = "2020-09-17 01:00:00";
-//    timer(T12,T12_ts1,T12_ts2);
-
-//    if(argc == 2 && strcmp( argv[1],"T10") == 0){
-//        cout<<"T10 start"<<endl;
-//        timer(T10, T10_ts1, T10_ts2);
-//    }
-//    else if (argc == 2 && strcmp( argv[1],"T11") == 0){
-//        cout<<"T11 start"<<endl;
-//        timer(T11, earthquake_X);
-//    }
-//    else if (argc == 2 && strcmp( argv[1],"T12") == 0){
-//        cout<<"T12 start"<<endl;
-//        timer(T12,T12_ts1,T12_ts2);
-//    }
-
-//    else timer(T11, earthquake_X);
-
-//    string T12_ts1 = "2020-09-17 00:00:00";
-//    string T12_ts2 = "2020-09-17 01:00:00";
-//            timer(T12,T12_ts1,T12_ts2);
-//    timer(T2);
+   // string T12_ts1 = "2020-09-17 00:00:00";
+   // string T12_ts2 = "2020-09-17 01:00:00";
+   // timer(T12,T12_ts1,T12_ts2);
 
 
-//    if(argc == 2 && strcmp( argv[1],"T1") == 0){
-//        timer(T1, curdate);
-//    }
-//    else if (argc == 2 && strcmp( argv[1],"T2") == 0){
-//        timer(T2);
-//    }
-//    else if (argc == 2 && strcmp( argv[1],"T6") == 0){
-//        timer(T6, 9);
-//    }
-//    else if (argc == 2 && strcmp( argv[1],"T16") == 0){
-//        timer(T16, ts);
-//    }
+    //    if(argc == 2 && strcmp( argv[1],"T1") == 0){
+    //        timer(T1, curdate);
+    //    }
+    //    else if (argc == 2 && strcmp( argv[1],"T2") == 0){
+    //        timer(T2);
+    //    }
+    //    else if (argc == 2 && strcmp( argv[1],"T6") == 0){
+    //        timer(T6, 9);
+    //    }
+    //    else if (argc == 2 && strcmp( argv[1],"T16") == 0){
+    //        timer(T16, ts);
+    //    }
     //timer(T6, 9);
     //
-     // if(argc == 2 && strcmp( argv[1],"T7") == 0){
-     //     timer(T7, 9);
-     // }
-     // else if (argc == 2 && strcmp( argv[1],"T8") == 0){
-     //     timer(T8, 9);
-     // }
-     // else if (argc == 2 && strcmp( argv[1],"T9") == 0){
-     //     timer(T9, 9);
-     // }
-//         timer(T8,9);
-//    long ts = 1600182000+10800*3.5;
-//    timer(T16, ts);
+    // if(argc == 2 && strcmp( argv[1],"T7") == 0){
+    //     timer(T7, 9);
+    // }
+    // else if (argc == 2 && strcmp( argv[1],"T8") == 0){
+    //     timer(T8, 9);
+    // }
+    // else if (argc == 2 && strcmp( argv[1],"T9") == 0){
+    //     timer(T9, 9);
+    // }
+    //         timer(T9,9);
+    //    long ts = 1600182000+10800*3.5;
+    //    timer(T16, ts);
 
-<<<<<<< HEAD
-//    string curdate3 = "2018-07-07";//"2021-12-25";
-//    string pid = "B007SYGLZO";
-//    string curdate = "2021-06-01";
-//    if(argc == 2 && strcmp( argv[1],"T3") == 0){
-//        timer(T3, curdate3);
-//    }
-//    else if (argc == 2 && strcmp( argv[1],"T4") == 0){
-//    timer(T4,10000,10);
-//    }
-//    else if (argc == 2 && strcmp( argv[1],"T5") == 0){
-//        timer(T5,pid, curdate);
-//    }
-//    //timer(T3,curdate3);
-=======
-    //string curdate3 = "2018-07-07";//"2021-12-25";
+
+    //  string curdate3 = "2018-07-07";//"2021-12-25";
     //string pid = "B007SYGLZO";
     //string curdate = "2021-06-01";
     //if(argc == 2 && strcmp( argv[1],"T3") == 0){
-    //    timer(T3, curdate3);
+    //      timer(T3, curdate3);
     //}
     //else if (argc == 2 && strcmp( argv[1],"T4") == 0){
-//    timer(T4,10000,10);
+    //    timer(T4,10000,10);
     //}
     //else if (argc == 2 && strcmp( argv[1],"T5") == 0){
-    //    timer(T5,pid, curdate);
+    //        timer(T5,pid, curdate);
     //}
     //timer(T3,curdate3);
->>>>>>> e9426e831660e6a6629a1b7b8a806ebfd55b506a
 
       return 0;
 }
