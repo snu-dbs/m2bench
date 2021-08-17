fd0c299f3c8ff36c43fe27a12798e6fe98b96d7e
Undraa Enkhbat <undraae@dbs.snu.ac.kr>
2021-08-17 07:40:54 +0000

    Upload New File

---
+++ src/Generator/script/Healthcare/main.py
@@ -0,0 +1,13 @@
+import mergedrug
+import graphprocess
+import documentprocess
+import tableprocess
+
+#import time
+
+if __name__ == "__main__":
+
+    mergedrug
+    graphprocess
+    documentprocess
+    tableprocess
