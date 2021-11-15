c9a3d4a364574bc081e663ea4826905bbe150301
kbgksj <bgkim@dbs.snu.ac.kr>
2021-11-15 18:32:44 +0900

    remove private info

--- Impl/agensgraph/load_datasets/healthcare/load_json.sql
+++ Impl/agensgraph/load_datasets/healthcare/load_json.sql
@@ -1,3 +1,3 @@
 \c healthcare
   
-COPY Drug (data) FROM program 'sed -e ''s/\\/\\\\/g'' /home/mxmdb/m2bench/data/healthcare/json/drug.json';
+COPY Drug (data) FROM program 'sed -e ''s/\\/\\\\/g'' /healthcare/json/drug.json';
