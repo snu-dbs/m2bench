528988b124e6c21b99ae3f878532ef2da582f144
kbgksj <bgkim@dbs.snu.ac.kr>
2021-11-30 18:48:57 +0900

    update script for agensgraph

--- Impl/agensgraph/load_datasets/ecommerce/load_json.sql
+++ Impl/agensgraph/load_datasets/ecommerce/load_json.sql
@@ -1,10 +1,10 @@
 \c ecommerce;
   
-COPY "order" (data) FROM '/ecommerce/json/order.json';
+COPY "order" (data) FROM '../../../../Datasets/ecommerce/json/order.json';
 CREATE INDEX order_customer_id_idx ON "order"((data->>'customer_id'));
 CREATE INDEX order_order_id_idx ON "order"((data->>'order_id'));
 CREATE INDEX order_orderline_product_id_idx ON "order"((data->'order_line'->>'product_id'));
 CREATE INDEX order_orderline_product_id_idx2 ON "order" using ((data->'order_line'));
-COPY review (data) FROM '/ecommerce/json/review.json';
+COPY review (data) FROM '../../../../Datasets/ecommerce/json/review.json';
 CREATE INDEX review_order_id_idx ON review((data->>'order_id'));
 CREATE INDEX review_product_id_idx ON review((data->>'product_id'));
--- Impl/agensgraph/load_datasets/ecommerce/load_table.sql
+++ Impl/agensgraph/load_datasets/ecommerce/load_table.sql
@@ -3,9 +3,10 @@
 
 SET DATASET_PAT 
 
-COPY brand FROM '/ecommerce/table/Brand.csv' DELIMITER ',' CSV HEADER; 
+COPY brand FROM '../../../../../Datasets/ecommerce/table/Brand.csv' DELIMITER ',' CSV HEADER; 
 
-COPY product FROM '/ecommerce/table/Product.csv' DELIMITER ',' CSV HEADER;
+COPY product FROM '../../../../Datasets/ecommerce/table/Product.csv' DELIMITER ',' CSV HEADER;
 
-COPY customer FROM '/ecommerce/table/Customer.csv' DELIMITER '|' CSV HEADER;
+COPY customer FROM '../../../../Datasets/ecommerce/table/Customer.csv' DELIMITER '|' CSV HEADER;
+
 
