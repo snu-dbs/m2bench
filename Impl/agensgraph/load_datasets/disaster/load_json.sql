a719ad615d92bdeabcd26db9d667539fab8da125
MxMDB <mxmdb@dbs.snu.ac.kr>
2022-09-18 15:32:07 +0900

    index / data load

--- Impl/agensgraph/load_datasets/disaster/load_json.sql
+++ Impl/agensgraph/load_datasets/disaster/load_json.sql
@@ -4,3 +4,9 @@ COPY Site (data) FROM program 'sed -e ''s/\\/\\\\/g'' /disaster/json/Site.json';
 CREATE INDEX Site_geometry_idx ON Site USING GIST(ST_GeomFromGeoJSON(data ->> 'geometry'::text));
 CREATE UNIQUE INDEX Site_id_idx ON Site USING btree (((data->>'site_id')::int));
 CREATE INDEX Site_type_idx On Site USING btree (((data->'properties'->>'type')::text));
+
+
+COPY Site_centroid (data) FROM program 'sed -e ''s/\\/\\\\/g'' /home/mxmdb/m2bench/data/disaster/json/site_centroid_wo_id.json';
+CREATE INDEX Site_centroid_geometry_idx ON Site_centroid USING GIST((ST_GeomFromGeoJSON(data ->> 'centroid'::text)::geography));
+CREATE UNIQUE INDEX Site_centroid_id_idx ON Site_centroid USING btree (((data->>'site_id')::int));
+CREATE INDEX Site_centroid_type_idx On Site_centroid USING btree (((data->'properties'->>'type')::text));
