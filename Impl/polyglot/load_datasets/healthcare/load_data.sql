5fc5a2d3671cb51dea8cb855f2beaee8442580fe
Kyoseung Koo <koo@dbs.snu.ac.kr>
2024-02-24 19:16:56 +0900

    fix polyglot import script

--- Impl/polyglot/load_datasets/healthcare/load_data.sql
+++ Impl/polyglot/load_datasets/healthcare/load_data.sql
@@ -1,5 +1,12 @@
 USE Healthcare;
 
+LOAD DATA LOCAL INFILE '../../../Datasets/healthcare/table/Patient.csv' 
+INTO TABLE Patient
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
 LOAD DATA LOCAL INFILE '../../../Datasets/healthcare/table/Prescription.csv' 
 INTO TABLE Prescription
 FIELDS TERMINATED BY ',' 
@@ -9,13 +16,6 @@ IGNORE 1 ROWS;
 
 LOAD DATA LOCAL INFILE '../../../Datasets/healthcare/table/Diagnosis.csv' 
 INTO TABLE Diagnosis
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
-
-LOAD DATA LOCAL INFILE '../../../Datasets/healthcare/table/Patient.csv' 
-INTO TABLE Patient
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
--- Impl/polyglot/load_datasets/healthcare/load_json.sh
+++ Impl/polyglot/load_datasets/healthcare/load_json.sh
@@ -1,2 +1,2 @@
 DATASET_PATH=../../../Datasets
-mongoimport --db Healthcare  --collection drug --drop --file $DATASET_PATH/healthcare/json/drug.json  --jsonArray;
+mongoimport --db Healthcare  --collection drug --drop --file $DATASET_PATH/healthcare/json/drug.json
