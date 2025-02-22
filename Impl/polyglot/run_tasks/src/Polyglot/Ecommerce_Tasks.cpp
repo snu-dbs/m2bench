96198bc56e6335b8c18ac29ea9a24508dd4bf608
yoojin-dbs <cyj@dbs.snu.ac.kr>
2025-02-22 13:09:01 +0900

    fix time_comm

--- Impl/polyglot/run_tasks/src/Polyglot/Ecommerce_Tasks.cpp
+++ Impl/polyglot/run_tasks/src/Polyglot/Ecommerce_Tasks.cpp
@@ -29,7 +29,7 @@ using std::chrono::milliseconds;
  */
 void T0(int brand_id)
 {
-    auto time_comm = high_resolution_clock::now();
+    auto time_comm = milliseconds(0);
     auto start_comm = high_resolution_clock::now();
     auto end_comm = high_resolution_clock::now();
 
